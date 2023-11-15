a03c0a1d58756a455c4a29d9ee03f05ec33d26f7
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-11-15 14:00:43 +0000

    clase privada y perimetro en circulo

--- circulo.h
+++ circulo.h
@@ -1,7 +1,15 @@
 #include <cmath>
 
 namespace Geometria {
+    class Circulo {
+        private:
+        double radio;
+        public:
     double calcularAreaCirculo(double radio) {
         return M_PI * radio * radio;
     }
+    double calcularPerimetroCirculo(double radio) {
+        return 2 * M_PI * radio;
+    }
+    };
 }
