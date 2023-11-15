c17602517805fd26953bd730fde3ae33c7b390ac
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-11-15 14:21:53 +0000

    ciencia quimica y matematicas

--- ciencia.h
+++ ciencia.h
@@ -1,8 +1,24 @@
+#include <iostream>
+
 namespace Ciencia {
     namespace Fisica {
         const double c = 299792458;
         double calcularEnergia(double masa) {
             return masa * c * c;
+        }
+    }
+    namespace Quimica {
+        void reaccionQuimica() {
+            std::cout << "Reaccion quimica" << std::endl;
+        }
+    }
+    namespace Matematicas {
+         int factorial(int n) {
+            if (n == 0 || n == 1) {
+                return 1;
+            } else {
+                return n * factorial(n - 1);
+            }
         }
     }
 }
